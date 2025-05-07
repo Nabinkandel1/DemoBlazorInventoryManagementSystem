37d6ad0f7b7a6894056ca06b394701518a93098b
Nabin Kandel <36199063+Nabinkandel1@users.noreply.github.com>
2025-05-07 09:02:20 +0545

    Day3 Create Interfaces For Product And Category Model

---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(int id);
+        Task<List<Product>> GetAllAsync();
+        Task  AddAsync(Product product);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(Product product);
+        Task<List<Product>> GetLowStockAsync(int threshold);
+    }
+}
